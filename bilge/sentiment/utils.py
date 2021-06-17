593daf3d94ab82b516ea23815ac18f3e8d4f5b0c
humanova <craxes_humanova@hotmail.com>
2021-06-18 01:31:20 +0300

    fix 'preprocess()' : remove/modify words from text to improve nlp analysis

--- bilge/sentiment/utils.py
+++ bilge/sentiment/utils.py
@@ -1,9 +1,24 @@
 # (c) 2021 Emir Erbasan (humanova)
+import logging
+
+ignore_list = ['http', 'www.', '---']
+modify_list = [('@', '@user')]
+
 
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
-        t = '@user' if t.startswith('@') and len(t) > 1 else t
-        t = 'http' if t.startswith('http') else t
-        new_text.append(t)
+        curr_t = None
+
+        for c, m in modify_list:
+            if t.startswith(c):
+                curr_t = m
+                break
+
+        for ig_text in ignore_list:
+            if t.startswith(ig_text):
+                curr_t = ''
+                break
+
+        new_text.append(t) if curr_t is None else new_text.append(curr_t)
     return " ".join(new_text)
