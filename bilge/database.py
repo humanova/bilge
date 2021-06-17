943a04fcc5c906c348ca2c15fff30d5cee8d1381
humanova <craxes_humanova@hotmail.com>
2021-06-18 01:33:24 +0300

    use 'on conflict update' instead of 'ignore', add methods to delete 'sentiment' and 'nlp inapplicability' rows

--- bilge/database.py
+++ bilge/database.py
@@ -3,7 +3,7 @@
 from datetime import datetime, timedelta
 
 from psycopg2 import *
-from peewee import * 
+from peewee import *
 from playhouse.shortcuts import model_to_dict
 
 import bilge
@@ -11,7 +11,7 @@ from bilge.logger import logging
 
 config = bilge.config
 
-db = None # BilgeDB instance
+db = None  # BilgeDB instance
 bilge_db = PostgresqlDatabase(config.db_name,
                               user=config.db_user,
                               password=config.db_password,
@@ -31,25 +31,31 @@ class Posts(Model):
     timestamp = IntegerField()
     score = IntegerField()
     language = CharField()
+
     class Meta:
         database = bilge_db
         db_table = 'posts'
+
 
 class Sentiment(Model):
     post_id = ForeignKeyField(Posts, backref='sentiment', unique=True)
     positive = DoubleField()
     neutral = DoubleField(null=True)
     negative = DoubleField()
+
     class Meta:
         database = bilge_db
         db_table = 'sentiment'
 
+
 class NLPInapplicability(Model):
     post_id = ForeignKeyField(Posts, backref='nlp_inapplicability', unique=True)
+
     class Meta:
         database = bilge_db
         db_table = 'nlp_inapplicability'
-    
+
+
 class BilgeDB:
     def __init__(self):
         try:
@@ -60,7 +66,7 @@ class BilgeDB:
         except Exception as e:
             logging.fatal(f"[DB] Couldn't connect to db")
             raise e
-    
+
     def init_tables(self):
         try:
             self.db.create_tables([Sentiment, NLPInapplicability])
@@ -68,63 +74,92 @@ class BilgeDB:
             logging.warning("[DB] Couldn't create tables (or tables already exist).")
             logging.warning(e)
 
-    def add_post_sentiment(self, post_id, positive, neutral, negative):
-        try: 
-           with self.db.atomic():
-                post = Sentiment.create(
-                    post_id= post_id,
-                    positive= positive,
-                    neutral= neutral,
-                    negative= negative
-                )
-                return post
-        except Exception as e:
-            logging.warning(f"[DB] Couldn't insert post : {e}")
-    
-    def add_post_sentiments(self, sentiments): 
+    def add_post_sentiment(self, sentiment):
         try:
             with self.db.atomic():
-                Sentiment.insert_many(sentiments).on_conflict_ignore().execute()
+                (Sentiment
+                 .insert(**sentiment)
+                 .on_conflict(
+                    conflict_target=[Sentiment.post_id],
+                    preserve=[Sentiment.post_id],
+                    update={Sentiment.positive: sentiment['positive'],
+                            Sentiment.neutral: sentiment['neutral'],
+                            Sentiment.negative: sentiment['negative']})
+                 .execute())
+        except Exception as e:
+            logging.warning(f"[DB] Couldn't insert post : {e}")
+
+    def add_post_sentiments(self, sentiments):
+        try:
+            with self.db.atomic():
+                for s in sentiments:
+                    (Sentiment
+                     .insert(**s)
+                     .on_conflict(
+                        conflict_target=[Sentiment.post_id],
+                        preserve=[Sentiment.post_id],
+                        update={Sentiment.positive: s['positive'],
+                                Sentiment.neutral: s['neutral'],
+                                Sentiment.negative: s['negative']})
+                     .execute())
         except Exception as e:
             logging.warning(f"[DB] Couldn't insert sentiments : {e}")
             logging.warning(f"sentiment data : {sentiments}")
 
-    def get_posts_without_sentiment(self, limit:int, before_date=None):
+    def delete_post_sentiments(self, post_ids):
+        try:
+            # posts = NLPInapplicability.select().join().where(NLPInapplicability.post_id << post_ids)
+            with self.db.atomic():
+                Sentiment.delete().where(Sentiment.post_id.in_(post_ids)).execute()
+        except Exception as e:
+            logging.warning(f"[DB] Couldn't delete sentiments : {e}")
+            logging.warning(f"post ids : {post_ids}")
+
+    def get_posts_without_sentiment(self, limit: int, before_date=None):
         before_date = datetime.utcnow() - timedelta(hours=1) if before_date is None else before_date
         try:
             posts = (Posts
                      .select(Posts.id, Posts.source, Posts.title, Posts.text, Posts.language)
                      .join_from(Posts, Sentiment, JOIN.LEFT_OUTER)
                      .join_from(Posts, NLPInapplicability, JOIN.LEFT_OUTER)
-                     .where((Posts.created_at < before_date) 
-                            & (Posts.language.is_null(False)) 
-                            & (Posts.language != '') 
+                     .where((Posts.created_at < before_date)
+                            & (Posts.language.is_null(False))
+                            & (Posts.language != '')
                             & (Sentiment.id.is_null())
                             & (NLPInapplicability.id.is_null()))
                      .limit(limit)
                      )
             return posts
-        except Exception as e: 
+        except Exception as e:
             logging.warning(f"[DB] Couldn't query the posts without sentiment : {e}")
 
-    def add_post_inapplicability(self, post_id):
-        try: 
-           with self.db.atomic():
-                post = NLPInapplicability.create(
-                    post_id= post_id
-                )
-                return post
+    def add_post_nlpinapplicability(self, post_id):
+        try:
+            with self.db.atomic():
+                NLPInapplicability.insert(
+                    post_id=post_id
+                ).on_conflict_ignore().execute()
         except Exception as e:
             logging.warning(f"[DB] Couldn't insert inapplicable post id : {e}")
 
-    def add_post_inapplicabilities(self, posts): 
+    def add_post_nlpinapplicabilities(self, posts):
         try:
             with self.db.atomic():
                 NLPInapplicability.insert_many(posts).on_conflict_ignore().execute()
         except Exception as e:
             logging.warning(f"[DB] Couldn't insert inapplicable post ids : {e}")
             logging.warning(f"posts data : {posts}")
-    
+
+    def delete_post_nlpinapplicabilities(self, post_ids):
+        try:
+            # posts = NLPInapplicability.select().join().where(NLPInapplicability.post_id << post_ids)
+            with self.db.atomic():
+                NLPInapplicability.delete().where(NLPInapplicability.post_id.in_(post_ids)).execute()
+        except Exception as e:
+            logging.warning(f"[DB] Couldn't delete nlp inapplicable posts : {e}")
+            logging.warning(f"post ids : {post_ids}")
+
+
 def post_to_dict(post):
     return model_to_dict(post)
 
