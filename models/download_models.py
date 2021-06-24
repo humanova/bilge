6e4f912c6d37037b52be9be4d2fd62ff3668bfb7
humanova <craxes_humanova@hotmail.com>
2021-06-25 00:30:42 +0300

    add english NER model to download_models script

--- models/download_models.py
+++ models/download_models.py
@@ -1,10 +1,12 @@
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import subprocess
+import sys
 
 SENTIMENT_EN = "cardiffnlp/twitter-roberta-base-sentiment"
 SENTIMENT_TR = "savasy/bert-base-turkish-sentiment-cased"
+NER_EN = "en_core_web_trf"
 
-
-def dl_pretrained_models(models):
+def dl_pretrained_hf_models(models):
     for m in models:
         tokenizer = AutoTokenizer.from_pretrained(m)
         model = AutoModelForSequenceClassification.from_pretrained(m)
@@ -12,8 +14,16 @@ def dl_pretrained_models(models):
         tokenizer.save_pretrained(m)
         model.save_pretrained(m)
 
+def dl_pretrained_spacy_models(models):
+    for m in models:
+        subprocess.check_call([sys.executable, "-m", "spacy", "download", m])
 
 if __name__ == "__main__":
-    mls = [SENTIMENT_EN, SENTIMENT_TR]
-    print(f"downloading the models : {', '.join(mls)}...")
-    dl_pretrained_models(mls)
+    hf_mls = [SENTIMENT_EN, SENTIMENT_TR]
+    sp_mls = [NER_EN]
+
+    print(f"downloading huggingface models : {', '.join(hf_mls)}...")
+    dl_pretrained_hf_models(hf_mls)
+
+    print(f"downloading spacy models : {', '.join(hf_mls)}...")
+    dl_pretrained_spacy_models(sp_mls)
